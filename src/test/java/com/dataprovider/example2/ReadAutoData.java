90540089986f30d60f6eac0ca36cc258e36d7743
Garia, Pawan (GfK) <Pawan.Garia@gfk.com>
2020-09-17 10:11:56 +0200

    Fixed and Updated for the Multiple Test methods passing Custome Indexes and All Methods Run with their own DataProvider

--- src/test/java/com/dataprovider/example2/ReadAutoData.java
+++ src/test/java/com/dataprovider/example2/ReadAutoData.java
@@ -1,26 +1,41 @@
 package com.dataprovider.example2;
 
 import org.testng.ITestContext;
+import org.testng.ITestNGMethod;
 import org.testng.annotations.DataProvider;
 import java.lang.reflect.Method;
 
 public class ReadAutoData {
 
     @DataProvider(name = "getData")
-    public static Object[][] getData(ITestContext context) {
-        Method method = context.getAllTestMethods()[0].getConstructorOrMethod().getMethod();
-        Object[][] arrayObject = new Object[][]{
+    public Object[][] getData(Method currentTestMethod, ITestContext context) {
+        Object[][] arrayObject = getTestData();
+        ITestNGMethod[] iTestNGMethods = context.getAllTestMethods();
+        for (ITestNGMethod iTestNGMethod : iTestNGMethods) {
+            Method method = iTestNGMethod.getConstructorOrMethod().getMethod();
+            if (method.getName().equalsIgnoreCase(currentTestMethod.getName())) {
+                if (method.isAnnotationPresent(IndexAnnotation.class)) {
+                    IndexAnnotation indexes = method.getAnnotation(IndexAnnotation.class);
+                    System.out.println("Creating the DataProvider between Start Index: " + indexes.startIndex() + " End Index: " + indexes.endIndex());
+                    return java.util.Arrays.copyOfRange(arrayObject, indexes.startIndex(), indexes.endIndex());
+                }
+            }
+        }
+        return arrayObject;
+    }
+
+    /**
+     * Get the By Default Data for the Data Provider.
+     *
+     * @return
+     */
+    private Object[][] getTestData() {
+        return new Object[][]{
                 {"user1", "password1"},
                 {"user2", "password2"},
                 {"user3", "password3"},
                 {"user4", "password4"},
                 {"user5", "password5"},
                 {"user6", "password6"}};
-        if (method.isAnnotationPresent(IndexAnnotation.class)) {
-            IndexAnnotation indexes = method.getAnnotation(IndexAnnotation.class);
-            System.out.println("Creating the DataProvider between Start Index: " + indexes.startIndex() + " End Index: " + indexes.endIndex());
-            return java.util.Arrays.copyOfRange(arrayObject, indexes.startIndex(), indexes.endIndex());
-        } else
-            return arrayObject;
     }
 }
--- src/test/java/com/dataprovider/example2/TestDataProvider.java
+++ src/test/java/com/dataprovider/example2/TestDataProvider.java
@@ -4,14 +4,20 @@ import org.testng.annotations.Test;
 
 public class TestDataProvider {
 
-    @Test(dataProvider = "getData",dataProviderClass = ReadAutoData.class)
+    @Test(dataProvider = "getData", dataProviderClass = ReadAutoData.class)
     public void normalTest(String userName, String password) {
-        System.out.println("Running Test for " + userName + " and " + password);
+        System.out.println("Running Normal Test for " + userName + " and " + password);
     }
 
-    @IndexAnnotation(startIndex = 0,endIndex = 4)
-    @Test(dataProvider = "getData",dataProviderClass = ReadAutoData.class)
+    @IndexAnnotation(startIndex = 0, endIndex = 4)
+    @Test(dataProvider = "getData", dataProviderClass = ReadAutoData.class)
     public void withCustomIndex(String userName, String password) {
-        System.out.println("Running Custom Test for " + userName + " and " + password);
+        System.out.println("Running Custom Test [0 -> 4 ] for " + userName + " and " + password);
+    }
+
+    @IndexAnnotation(startIndex = 0, endIndex = 2)
+    @Test(dataProvider = "getData", dataProviderClass = ReadAutoData.class)
+    public void withCustomIndex2(String userName, String password) {
+        System.out.println("Running Custom Test [0 -> 2 ] for " + userName + " and " + password);
     }
 }
